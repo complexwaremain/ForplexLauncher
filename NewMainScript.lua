5c596c0dbe743536ecc6a0559e002f5d0593f61e
Complex <166940834+complexwaremain@users.noreply.github.com>
2024-07-12 21:11:16 +0400

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -60,31 +60,31 @@ if not shared.VapeDeveloper then
 	end
 	if commit then
 		if isfolder("vape") then 
-			if ((not isfile("vape/commithash.txt")) or (readfile("vape/commithash.txt") ~= commit or commit == "main")) then
-				for i,v in pairs({"vape/Universal.lua", "vape/MainScript.lua", "vape/GuiLibrary.lua"}) do 
+			if ((not isfile("Forplex1/commithash.txt")) or (readfile("Forplex1/commithash.txt") ~= commit or commit == "main")) then
+				for i,v in pairs({"Forplex1/Universal.lua", "Forplex1/MainScript.lua", "Forplex1/GuiLibrary.lua"}) do 
 					if isfile(v) and ({readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.")})[1] == 1 then
 						delfile(v)
 					end 
 				end
-				if isfolder("vape/CustomModules") then 
-					for i,v in pairs(listfiles("vape/CustomModules")) do 
+				if isfolder("Forplex1/CustomModules") then 
+					for i,v in pairs(listfiles("Forplex1/CustomModules")) do 
 						if isfile(v) and ({readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.")})[1] == 1 then
 							delfile(v)
 						end 
 					end
 				end
-				if isfolder("vape/Libraries") then 
-					for i,v in pairs(listfiles("vape/Libraries")) do 
+				if isfolder("Forplex1/Libraries") then 
+					for i,v in pairs(listfiles("Forplex1/Libraries")) do 
 						if isfile(v) and ({readfile(v):find("--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.")})[1] == 1 then
 							delfile(v)
 						end 
 					end
 				end
-				writefile("vape/commithash.txt", commit)
+				writefile("Forplex1/commithash.txt", commit)
 			end
 		else
 			makefolder("vape")
-			writefile("vape/commithash.txt", commit)
+			writefile("Forplex1/commithash.txt", commit)
 		end
 	else
 		displayErrorPopup("Failed to connect to github, please try using a VPN.")
